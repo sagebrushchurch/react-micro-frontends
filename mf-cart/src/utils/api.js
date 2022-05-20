b70a157d895c72c7ce31715195269c61e9c77dab
Jon Krake <1024jon@gmail.com>
2022-05-20 13:08:49 -0600

    try https

--- mf-cart/src/utils/api.js
+++ mf-cart/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const call = method => (url, { body, query, headers } = {}) => {
   return axios({
     method,
-    baseURL: `http://microfe.sagebrush.dev/backend${url}`,
+    baseURL: `https://microfe.sagebrush.dev/backend${url}`,
     data: body,
     headers,
     params: query
