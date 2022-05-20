e922ad01e5c3050621d6ca9d81c574c7f423ad4f
Nick Young <nickyoungweb@gmail.com>
2022-05-20 15:05:54 -0600

    Add account nav item

--- host-app/src/components/Navbar/index.jsx
+++ host-app/src/components/Navbar/index.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from "react";
 import { useHistory } from "react-router-dom";
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import PetsIcon from '@material-ui/icons/Pets';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import PetsIcon from "@material-ui/icons/Pets";
 
-import useStyles from './styles';
-import Theme from '../Theme';
-import CartBadge from '../CartBadge';
+import useStyles from "./styles";
+import Theme from "../Theme";
+import CartBadge from "../CartBadge";
 
 const Navbar = ({ isCartLoading, cart }) => {
   let history = useHistory();
   const classes = useStyles();
-  const showCartBadge = history.location.pathname === '/';
+  const showCartBadge = history.location.pathname === "/";
 
-  const handleClick = () => history.push('/');
+  const handleClick = () => history.push("/");
 
   return (
     <div className={classes.root}>
@@ -26,19 +26,21 @@ const Navbar = ({ isCartLoading, cart }) => {
             edge="start"
             className={classes.menuButton}
             color="inherit"
-            aria-label="menu">
+            aria-label="menu"
+          >
             <PetsIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             IT Store
           </Typography>
           <div className={classes.grow} />
+          <a href="/account">My Account</a>
           {showCartBadge && <CartBadge cart={cart} />}
           <Theme />
         </Toolbar>
       </AppBar>
     </div>
   );
-}
+};
 
 export default Navbar;
--- host-app/src/utils/config.js
+++ host-app/src/utils/config.js
@@ -1,4 +1,5 @@
 export default {
   mfProductsUrl: process.env.REACT_APP_PRODUCTS_MICROFRONTEND_URL,
-  mfCartUrl: process.env.REACT_APP_CART_MICROFRONTEND_URL
-}
+  mfCartUrl: process.env.REACT_APP_CART_MICROFRONTEND_URL,
+  mfAccountUrl: process.env.REACT_APP_ACCOUNT_MICROFRONTEND_URL,
+};
