8302d1a48b537571767aaff603d827a8f8eebb63
Nick Young <nickyoungweb@gmail.com>
2022-05-20 16:09:12 -0600

    Update route to Account component

--- host-app/src/components/Body/index.jsx
+++ host-app/src/components/Body/index.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
 import { Switch, Route } from "react-router-dom";
 
-import Products from '../Products';
-import Cart from '../Cart';
-import useStyles from './styles';
+import Products from "../Products";
+import Cart from "../Cart";
+import Account from "../Account";
+import useStyles from "./styles";
 
 const Body = () => {
   const classes = useStyles();
   return (
     <main className={classes.main}>
-      <Grid className={classes.content} >
+      <Grid className={classes.content}>
         <Switch>
+          <Route path="/account">
+            <Account />
+          </Route>
           <Route path="/cart">
             <Cart />
           </Route>
