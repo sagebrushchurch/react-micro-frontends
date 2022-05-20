a7108965cbe06404504c6d17b544ff55eed0fb6b
Jon Krake <1024jon@gmail.com>
2022-05-20 12:57:06 -0600

    attempt to run remote

--- host-app/src/utils/api.js
+++ host-app/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const call = method => (url, { body, query, headers } = {}) => {
   return axios({
     method,
-    baseURL: `http://localhost:3003${url}`,
+    baseURL: `http://sagebrush.dev/backend${url}`,
     data: body,
     headers,
     params: query
